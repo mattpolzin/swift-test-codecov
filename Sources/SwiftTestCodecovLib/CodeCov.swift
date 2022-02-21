5f37c5a62860ec9abb555f00fa6f829b413b9a07
Ravi Tripathi <ravitripathi1996@gmail.com>
2022-02-22 00:38:26 +0530

    Refactored filter logic for tests to Aggregate init itself. Removed mutating functions added across structs

--- Sources/SwiftTestCodecovLib/CodeCov.swift
+++ Sources/SwiftTestCodecovLib/CodeCov.swift
@@ -10,7 +10,7 @@ import Foundation
 public struct CodeCov: Decodable {
     public let version: String
     public let type: String
-    public var data: [Data]
+    public let data: [Data]
 
     public func fileCoverages(for property: AggregateProperty) -> [String: File.Coverage] {
         return Dictionary(uniqueKeysWithValues: data
@@ -20,26 +20,8 @@ public struct CodeCov: Decodable {
         )
     }
 
-    /// Removes all files containing "Tests" or "Test" in their names from coverage result
-    public mutating func stripAllTestFiles() {
-        var processedData = [Data]()
-        for datum in data {
-            var datumCopy = datum
-            datumCopy.stripTestFiles()
-            processedData.append(datumCopy)
-        }
-        self.data = processedData
-    }
-
     public struct Data: Decodable {
-        public var files: [File]
-
-        /// Removes all files containing "Tests" or "Test" in their names from coverage result
-        mutating func stripTestFiles() {
-            files = files.filter({ file in
-                !(file.filename.lowercased().contains("tests") || file.filename.lowercased().contains("test"))
-            })
-        }
+        public let files: [File]
     }
 
     public struct File: Decodable {
